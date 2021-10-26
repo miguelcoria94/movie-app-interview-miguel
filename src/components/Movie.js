3e970629c2f593e0804466d8684ac4d7de75e409
miguelcoria94 <miguelcoria94@gmail.com>
2021-10-26 17:35:50 -0500

    change the movie name

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Movie.css";
 
 function Movie({ movieDetails }) {
-  return <div>{movieDetails.original_title}</div>;
+  return (
+    <div className="movie__container">
+      <h5 className="movie__name">{movieDetails.original_title}</h5>
+    </div>
+  );
 }
 
 export default Movie;
