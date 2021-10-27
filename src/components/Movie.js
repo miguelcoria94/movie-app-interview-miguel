cfb7d512f0b4b6836a650f784c3fc3c058749b0f
miguelcoria94 <miguelcoria94@gmail.com>
2021-10-26 20:21:10 -0500

    created the movie bg gradient

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,10 +9,16 @@ function Movie({ movieDetails }) {
       </div>
       <div className="movie__details">
         <div className="movie__details-accent"></div>
-        <span className="movie__rating">
-          <span className="fas fa-star movie__rating-icon"></span>{" "}
-          {movieDetails.vote_average}
-        </span>
+        <div>
+          <span className="movie__rating">
+            <span className="fas fa-star movie__rating-icon"></span>{" "}
+            {movieDetails.vote_average}
+          </span>
+          <span className="movie__vote_count">
+            <span className="fas fa-user movie__vote_count-icon"></span>{" "}
+            {movieDetails.vote_count}
+          </span>
+        </div>
         <h5 className="movie__name">{movieDetails.original_title}</h5>
         <span className="movie__overview">{movieDetails.overview}</span>
       </div>
