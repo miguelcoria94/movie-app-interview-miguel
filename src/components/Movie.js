fd0cca332ccd013e49332e6c6a6097a6342ec3f2
miguelcoria94 <miguelcoria94@gmail.com>
2021-10-26 20:29:33 -0500

    adjusted rating

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,17 +9,17 @@ function Movie({ movieDetails }) {
       </div>
       <div className="movie__details">
         <div className="movie__details-accent"></div>
+        <h5 className="movie__name">{movieDetails.original_title}</h5>
         <div>
           <span className="movie__rating">
             <span className="fas fa-star movie__rating-icon"></span>{" "}
             {movieDetails.vote_average}
           </span>
-          <span className="movie__vote_count">
-            <span className="fas fa-user movie__vote_count-icon"></span>{" "}
+          <span className="movie__vote-count">
+            <span className="fas fa-user movie__vote-count-icon"></span>{" "}
             {movieDetails.vote_count}
           </span>
         </div>
-        <h5 className="movie__name">{movieDetails.original_title}</h5>
         <span className="movie__overview">{movieDetails.overview}</span>
       </div>
     </div>
