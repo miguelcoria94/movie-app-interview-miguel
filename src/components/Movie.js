79f1f58f4cf317083b170b35ca56fe06ab0049d8
miguelcoria94 <miguelcoria94@gmail.com>
2021-10-26 20:04:55 -0500

    added rating icon

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,8 +8,13 @@ function Movie({ movieDetails }) {
         <img src="" alt={movieDetails.original_title} />
       </div>
       <div className="movie__details">
-        <span className="movie__details-accent"></span>
+        <div className="movie__details-accent"></div>
+        <span className="movie__rating">
+          <span className="fas fa-star movie__rating-icon"></span>{" "}
+          {movieDetails.vote_average}
+        </span>
         <h5 className="movie__name">{movieDetails.original_title}</h5>
+        <span className="movie__overview">{movieDetails.overview}</span>
       </div>
     </div>
   );
