cf02e705afec90fd3849ad8d2c5031067d329768
miguelcoria94 <miguelcoria94@gmail.com>
2021-10-26 20:33:50 -0500

    remove the accent

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,8 +8,6 @@ function Movie({ movieDetails }) {
         <img src="" alt={movieDetails.original_title} />
       </div>
       <div className="movie__details">
-        <div className="movie__details-accent"></div>
-        <h5 className="movie__name">{movieDetails.original_title}</h5>
         <div>
           <span className="movie__rating">
             <span className="fas fa-star movie__rating-icon"></span>{" "}
@@ -19,6 +17,7 @@ function Movie({ movieDetails }) {
             <span className="fas fa-user movie__vote-count-icon"></span>{" "}
             {movieDetails.vote_count}
           </span>
+          <span className="movie__name">{movieDetails.original_title}</span>
         </div>
         <span className="movie__overview">{movieDetails.overview}</span>
       </div>
